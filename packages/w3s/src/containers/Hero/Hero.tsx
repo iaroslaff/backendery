637c9d4b9e13bc39f17693a2f662ef2c98a2f7e7
Dimmasay <kruhliakds@gmail.com>
2024-05-15 02:57:58 +0300

    refactor (w3s): change styles the container `Hero`

--- packages/w3s/src/containers/Hero/Hero.tsx
+++ packages/w3s/src/containers/Hero/Hero.tsx
@@ -6,55 +6,52 @@ import "./Hero.scss"
 
 const Hero: FC = () => {
   return (
-    <React.Fragment>
-      <section className={"hero"}>
-        <div className={"hero__offer"}>
-          <div className={"hero__offer__description-wrapper"}>
-            <a className={"hero__offer__description-wrapper__btn-call-to-action"} href={"#"}>
-              Start
-              <br />a project
-            </a>
-            <p className={"hero__offer__description-wrapper__description"}>
-              Sed vestibulum tincidunt tellus, ut pulvinar sapien
-            </p>
-          </div>
-          <h1 className={"hero__offer__title"}>
-            <p className={"hero__offer__title__word"}>
-              Fastlw<span className={"hero__offer__title__word-decoration-dot"}>.</span>
-            </p>
-            <p className={"hero__offer__title__word"}>
-              Robust<span className={"hero__offer__title__word-decoration-dot"}>.</span>
-            </p>
-            <p className={"hero__offer__title__word"}>
-              Flexible<span className={"hero__offer__title__word-decoration-dot"}>.</span>
-            </p>
-          </h1>
+    <section className={"hero"}>
+      <div className={"hero__offer"}>
+        <div className={"hero__description-wrapper"}>
+          <button className={"hero__btn-call-to-action"}>
+            Start
+            <br />a project
+          </button>
+          <p className={"hero__description"}>Sed vestibulum tincidunt tellus, ut pulvinar sapien</p>
         </div>
-        <div className={"hero_elements"}>
-          <div className={"hero_code-container"}>
-            <div className={"code-container_decorate-line"}></div>
-            <p className={"hero_code"}>
+        <div className={"hero__title-wrapper"}>
+          <h2 className={"hero__title-word"}>
+            Fastlw<span>.</span>
+          </h2>
+          <h2 className={"hero__title-word"}>
+            Robust<span>.</span>
+          </h2>
+          <h2 className={"hero__title-word"}>
+            Flexible<span>.</span>
+          </h2>
+        </div>
+      </div>
+      <div className={"hero__code-container"}>
+        <div className={"hero__code-wrapper"}>
+          <div className={"hero__code hero__code--1"}>
+            <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec elit augue. Aenean pharetra lectus
               elementum felis vestibulum, at fringilla mi dictum. Etiam eget tellus ac sapien dignissim tincidunt quis
-              eget augue. Aenean semper felis mi. Pellentesque vel auctor magna. Ut non tortor ut quam vulputate
+              eget augue. Aenean semper felis mi. Pellentesque vel auctor magna. Ut non tortor
             </p>
           </div>
-          <div className={"hero_code-container hero_code-container-2"}>
-            <div className={"code-container_decorate-line"}></div>
-            <p className={"hero_code"}>
-              ugue. Aenean pharetra lectu mi dictum. Etiam eget tellus ac sapien dignissim tincidunt quis eget augue.
-              Aenean semper felis mi. Pellentesque vel auctor magna. Ut non tortor ut quam vulputate
+          <div className={"hero__code hero__code--2"}>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec elit augue. Aenean pharetra lectus
+              elementum felis vestibulum, at fringilla mi dictum. Etiam eget tellus ac sapien dignissim tincidunt quis
+              eget augue. Aenean semper felis mi. Pellentesque vel auctor magna. Ut non tortor
             </p>
-          </div>
-          <div className={"hero_elements_watch-down-container"}>
-            <span>Let&apos;s watch</span>
-            <div className={"hero_elements_watch-down-circle"}>
-              <SvgIcon name={"arrow-watch-white"} />
-            </div>
           </div>
         </div>
-      </section>
-    </React.Fragment>
+        <div className={"hero__lets-watch"}>
+          <span>Let&apos;s watch</span>
+          <button className={"hero__lets-watch-circle"}>
+            <SvgIcon name={"arrow-watch-white"} />
+          </button>
+        </div>
+      </div>
+    </section>
   )
 }
 

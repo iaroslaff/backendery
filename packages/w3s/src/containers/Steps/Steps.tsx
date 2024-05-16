b1442d62651fb83ffbd9afaa3adce0f8749f6cc9
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-05-16 18:08:57 +0300

    change steps

--- packages/w3s/src/containers/Steps/Steps.tsx
+++ packages/w3s/src/containers/Steps/Steps.tsx
@@ -35,51 +35,56 @@ const Steps: FC = () => {
         <span>Steps</span>
       </h2>
       <div className={"steps__container"}>
-        <div className={"steps__step steps__step--1"}>
-          <h4 className={"tile-step__title"}>
+        <div className={"steps__card steps__card--1"}>
+          <h4 className={"steps__card-title"}>
             <span>01.</span> Analysis
           </h4>
-          <p className={"tile-step__description"}>
+          <p className={"steps__card-description"}>
             At this stage, we analyze the task at hand, select the best ideas, solutions and tools to implement the
             project.
           </p>
         </div>
-        <div className={"steps__step steps__step--2"}>
-          <h4 className={"tile-step__title"}>
+
+        <div className={"steps__card steps__card--2"}>
+          <h4 className={"steps__card-title"}>
             <span>02.</span> Development
           </h4>
-          <p className={"tile-step__description"}>
+          <p className={"steps__card-description"}>
             At this stage, we analyze the task at hand, select the best ideas, solutions and tools to implement the
             project.
           </p>
         </div>
-        <div className={"steps__step steps__step--3"}>
-          <h4 className={"tile-step__title"}>
+
+        <div className={"steps__card steps__card--3"}>
+          <h4 className={"steps__card-title"}>
             <span>03.</span> Testing
           </h4>
-          <p className={"tile-step__description"}>
+          <p className={"steps__card-description"}>
             At this stage, we analyze the task at hand, select the best ideas, solutions and tools to implement the
             project.
           </p>
         </div>
-        <div className={"steps__step steps__step--4"}>
-          <h4 className={"tile-step__title"}>
+
+        <div className={"steps__card steps__card--4"}>
+          <h4 className={"steps__card-title"}>
             <span>04.</span> Stagning
           </h4>
-          <p className={"tile-step__description"}>
+          <p className={"steps__card-description"}>
             At this stage, we analyze the task at hand, select the best ideas, solutions and tools to implement the
             project.
           </p>
         </div>
-        <div className={"steps__step steps__step--5"}>
-          <h4 className={"tile-step__title"}>
+
+        <div className={"steps__card steps__card--5"}>
+          <h4 className={"steps__card-title"}>
             <span>05.</span> Release
           </h4>
-          <p className={"tile-step__description"}>
+          <p className={"steps__card-description"}>
             At this stage, we analyze the task at hand, select the best ideas, solutions and tools to implement the
             project.
           </p>
         </div>
+
         <p className={"steps__text"}>
           We always try <br /> to follow <span>the sequence of work</span>
         </p>

404f582e3c0701b695ded0b7eb2b623a2b3984e5
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-05-17 19:59:49 +0300

    wip

--- packages/w3s/src/layouts/Footer/Footer.tsx
+++ packages/w3s/src/layouts/Footer/Footer.tsx
@@ -1,37 +1,46 @@
-import React, { FC } from "react"
+import { FC } from "react"
 
-import { SvgIcon } from "./../../components/elements/Icon"
+import { SvgIcon } from "../../components/elements/Icon"
+import { useApp } from "../../contexts/App"
+
 import "./Footer.scss"
 
 const Footer: FC = () => {
+  const { setLetsStartedFormVisibility } = useApp()
+
   return (
-    <footer className='footer'>
-      <div className='footer__container'>
-        <div className='footer__body'>
-          <div className='footer__title'>
-            Let’s make something create <span></span> <br />
+    <footer className={"footer"}>
+      <div className={"footer__container"}>
+        <div className={"footer__body"}>
+          <div className={"footer__title"}>
+            Let&apos;s make something create <span></span> <br />
             <SvgIcon name={"arrow-offer-dark"} /> together
           </div>
-          <div className='footer__contacts'>
-            <a href='#' className='footer__contacts-mail'>
-              ask@mail.io
+          <div className={"footer__contacts"}>
+            <a href={"mailto:hey@backendery.dev"} className={"footer__contacts-email"}>
+              hey@backendery.dev
             </a>
-            <button className='footer__contacts-start-project-btn'>
+            <button
+              className={"footer__contacts-start-project-btn"}
+              onClick={() => {
+                setLetsStartedFormVisibility(true)
+              }}
+            >
               <span>Start a project</span>
             </button>
           </div>
         </div>
-        <div className='footer__bottom bottom-footer'>
-          <div className='bottom-footer__policy'>©2024, Backendery</div>
-          <ul className='bottom-footer__footer-links'>
-            <li className='bottom-footer__footer-link'>
-              <a href='#'>Telegram</a>
+        <div className={"footer__bottom bottom-footer"}>
+          <div className={"bottom-footer__policy"}>©{new Date().getFullYear()}, Backendery. All rights reserved.</div>
+          <ul className={"bottom-footer__footer-links"}>
+            <li className={"bottom-footer__footer-link"}>
+              <a href={"#"}>Telegram</a>
             </li>
-            <li className='bottom-footer__footer-link'>
-              <a href='#'>Linkedin</a>
+            <li className={"bottom-footer__footer-link"}>
+              <a href={"#"}>LinkedIn</a>
             </li>
-            <li className='bottom-footer__footer-link'>
-              <a href='#'>Facebook</a>
+            <li className={"bottom-footer__footer-link"}>
+              <a href={"#"}>Facebook</a>
             </li>
           </ul>
         </div>

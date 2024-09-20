6f6aa6c6e0f2da2e30b5db31ad78c5c47314dc56
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-09-20 21:16:29 +0300

    feat (w3s): add containers for `404` and `50X` errors

---
+++ packages/w3s/src/containers/errors/NotFoundError/NotFoundError.tsx
@@ -0,0 +1,43 @@
+import { FC, useEffect, useRef } from "react"
+import Typed from "typed.js"
+
+import "./NotFoundError.scss"
+
+const NotFoundError: FC = () => {
+  const tagResetErrorAnchorRef = useRef(null)
+
+  useEffect(() => {
+    const typed = new Typed(tagResetErrorAnchorRef && tagResetErrorAnchorRef.current, {
+      cursorChar: "_",
+      strings: ["return home?"],
+      typeSpeed: 90,
+    })
+
+    return () => {
+      typed.destroy()
+    }
+  }, [])
+
+  return (
+    <div className={"not-found-error"}>
+      <div className={"not-found-error__status-code"}>404</div>
+      <div
+        className={
+          "not-found-error__emulator-of-console-text not-found-error__emulator-of-console-text--console-border"
+        }
+      >
+        <div>{"#>"}&nbsp;uh-oh! page not found...</div>
+        <div>
+          {"#>"}&nbsp;<a ref={tagResetErrorAnchorRef} href={"/"}></a>
+        </div>
+      </div>
+      <div className={"not-found-error__what-next"}>
+        Go ahead then
+        <br />
+        Click the terminal message
+      </div>
+    </div>
+  )
+}
+
+export default NotFoundError

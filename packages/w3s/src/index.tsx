a943e324c780ac8b46b580dc3bbf25cf7cacc3d7
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-09-25 01:07:29 +0300

    refactor (w3s): move usage `ErrorBoundary` to the `Router`

--- packages/w3s/src/index.tsx
+++ packages/w3s/src/index.tsx
@@ -1,20 +1,15 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { ErrorBoundary } from "react-error-boundary"
 
 import App from "./App"
-import InternalServerError from "./containers/errors/InternalServerError/InternalServerError"
 
 import "./index.scss"
 
-const rootElement = document.querySelector("#root") as HTMLElement
+const rootElement = document.querySelector("#root") as HTMLDivElement
 const root = createRoot(rootElement)
 
 root.render(
   <StrictMode>
-    <ErrorBoundary FallbackComponent={InternalServerError}>
-      <App />
-    </ErrorBoundary>
+    <App />
   </StrictMode>
 )
-
--- packages/w3s/src/pages/Router.tsx
+++ packages/w3s/src/pages/Router.tsx
@@ -1,12 +1,22 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom"
 
+import InternalServerError from "../containers/errors/InternalServerError/InternalServerError"
 import DefaultLayout from "../layouts/DefaultLayout"
 import MainPage from "./MainPage"
 import NotFoundPage from "./NotFoundPage"
 
 const Router = createBrowserRouter(
   createRoutesFromElements(
-    <Route>
+    <Route
+      errorElement={
+        <InternalServerError
+          error={undefined}
+          resetErrorBoundary={function (...args: any[]): void {
+            document.location.reload()
+          }}
+        />
+      }
+    >
       <Route path={"/"} element={<DefaultLayout />}>
         <Route index={true} element={<MainPage />} />
       </Route>

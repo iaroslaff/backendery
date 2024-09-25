5ac3ddd34f75611cb4f01c53aaefe22105ac49d4
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-09-25 23:08:48 +0300

    chore (w3s): replace the stubs with real names

--- packages/w3s/src/containers/TiledWall/TiledWall.tsx
+++ packages/w3s/src/containers/TiledWall/TiledWall.tsx
@@ -5,11 +5,12 @@ import { addClass, removeClass } from "../../utils/fn"
 import "./TiledWall.scss"
 
 const TiledWall: FC = () => {
+  /** refs */
   const mainContainerRef = useRef<HTMLDivElement>(null)
-
   const tiledWallRef = useRef<HTMLDivElement>(null)
   const tilesRef = useRef<NodeListOf<HTMLDivElement> | null>(null)
 
+  /** states */
   const [positionX, setPositionX] = useState(0)
   const [positionY, setPositionY] = useState(0)
 
@@ -141,7 +142,6 @@ const TiledWall: FC = () => {
 
   useEffect(() => {
     const handleKeyDown = ({ key }: KeyboardEvent) => key === "Escape" && zoomIn()
-
     document.addEventListener("keydown", handleKeyDown)
 
     return () => {
@@ -154,82 +154,82 @@ const TiledWall: FC = () => {
       <div className='tiled-wall animate--none' ref={tiledWallRef}>
         {/* main tile */}
         <div className='tile' data-x-position='0' data-y-position='0'>
-          <span className='tile__navigate tile__navigate--up js-up'>up</span>
+          <span className='tile__navigate tile__navigate--up js-up'>we do</span>
           <span className='tile__navigate tile__navigate--right-top js-up js-right'>up/right</span>
-          <span className='tile__navigate tile__navigate--left-top js-up js-left'>up/left</span>
-          <span className='tile__navigate tile__navigate--left js-left'>left</span>
-          <span className='tile__navigate tile__navigate--right js-right'>right</span>
-          <span className='tile__navigate tile__navigate--right-down js-down js-right'>down/right</span>
-          <span className='tile__navigate tile__navigate--left-down js-down js-left'>down/left</span>
-          <span className='tile__navigate tile__navigate--down js-down'>down</span>
+          <span className='tile__navigate tile__navigate--left-top js-up js-left'>about us</span>
+          <span className='tile__navigate tile__navigate--left js-left'>cases</span>
+          <span className='tile__navigate tile__navigate--right js-right'>steps</span>
+          <span className='tile__navigate tile__navigate--right-down js-down js-right'>contacts</span>
+          <span className='tile__navigate tile__navigate--left-down js-down js-left'>let&apos;s start</span>
+          <span className='tile__navigate tile__navigate--down js-down'>we use</span>
           <span className='tile__zoom js-zoom' onClick={zoomOut}>
             View All
           </span>
           <h1>Main</h1>
         </div>
-        {/* 1 */}
+        {/* up */}
         <div className='tile' data-x-position='0' data-y-position='1'>
-          <span className='tile__navigate tile__navigate--left js-left'>left</span>
-          <span className='tile__navigate tile__navigate--right js-right'>right</span>
-          <span className='tile__navigate tile__navigate--right-down js-down js-right'>down/right</span>
-          <span className='tile__navigate tile__navigate--left-down js-down js-left'>down/left</span>
-          <span className='tile__navigate tile__navigate--down js-down'>down</span>
-          <h1>Up</h1>
+          <span className='tile__navigate tile__navigate--left js-left'>about us</span>
+          <span className='tile__navigate tile__navigate--right js-right'>up/right</span>
+          <span className='tile__navigate tile__navigate--right-down js-down js-right'>steps</span>
+          <span className='tile__navigate tile__navigate--left-down js-down js-left'>cases</span>
+          <span className='tile__navigate tile__navigate--down js-down'>main</span>
+          <h1>we do</h1>
         </div>
-        {/* 2 */}
+        {/* up/right */}
         <div className='tile' data-x-position='1' data-y-position='1'>
-          <span className='tile__navigate tile__navigate--left-down js-down js-left'>down/left</span>
-          <span className='tile__navigate tile__navigate--left js-left'>left</span>
-          <span className='tile__navigate tile__navigate--down js-down'>down</span>
-          <h1>Up Right</h1>
+          <span className='tile__navigate tile__navigate--left-down js-down js-left'>main</span>
+          <span className='tile__navigate tile__navigate--left js-left'>we do</span>
+          <span className='tile__navigate tile__navigate--down js-down'>steps</span>
+          <h1>up right</h1>
         </div>
-        {/* 3 */}
+        {/* right */}
         <div className='tile' data-x-position='1' data-y-position='0'>
-          <span className='tile__navigate tile__navigate--up js-up'>up</span>
-          <span className='tile__navigate tile__navigate--left-top js-up js-left'>up/left</span>
-          <span className='tile__navigate tile__navigate--left js-left'>left</span>
-          <span className='tile__navigate tile__navigate--left-down js-down js-left'>down/left</span>
-          <span className='tile__navigate tile__navigate--down js-down'>down</span>
-          <h1>Right</h1>
+          <span className='tile__navigate tile__navigate--up js-up'>up/right</span>
+          <span className='tile__navigate tile__navigate--left-top js-up js-left'>we do</span>
+          <span className='tile__navigate tile__navigate--left js-left'>main</span>
+          <span className='tile__navigate tile__navigate--left-down js-down js-left'>we use</span>
+          <span className='tile__navigate tile__navigate--down js-down'>contacts</span>
+          <h1>steps</h1>
         </div>
-        {/* 4 */}
+        {/* down/right */}
         <div className='tile' data-x-position='1' data-y-position='-1'>
-          <span className='tile__navigate tile__navigate--up js-up'>up</span>
-          <span className='tile__navigate tile__navigate--left-top js-up js-left'>up/left</span>
-          <span className='tile__navigate tile__navigate--left js-left'>left</span>
-          <h1>Down Right</h1>
+          <span className='tile__navigate tile__navigate--up js-up'>steps</span>
+          <span className='tile__navigate tile__navigate--left-top js-up js-left'>main</span>
+          <span className='tile__navigate tile__navigate--left js-left'>we use</span>
+          <h1>contacts</h1>
         </div>
-        {/* 5 */}
+        {/* down */}
         <div className='tile' data-x-position='0' data-y-position='-1'>
-          <span className='tile__navigate tile__navigate--up js-up'>up</span>
-          <span className='tile__navigate tile__navigate--left-top js-up js-left'>up/left</span>
-          <span className='tile__navigate tile__navigate--right-top js-up js-right'>up/right</span>
-          <span className='tile__navigate tile__navigate--left js-left'>left</span>
-          <span className='tile__navigate tile__navigate--right js-right'>right</span>
-          <h1>Down</h1>
+          <span className='tile__navigate tile__navigate--up js-up'>main</span>
+          <span className='tile__navigate tile__navigate--left-top js-up js-left'>cases</span>
+          <span className='tile__navigate tile__navigate--right-top js-up js-right'>steps</span>
+          <span className='tile__navigate tile__navigate--left js-left'>let&apos;s start</span>
+          <span className='tile__navigate tile__navigate--right js-right'>contacts</span>
+          <h1>we use</h1>
         </div>
-        {/* 6 */}
+        {/* down/left */}
         <div className='tile' data-x-position='-1' data-y-position='-1'>
-          <span className='tile__navigate tile__navigate--up js-up'>up</span>
-          <span className='tile__navigate tile__navigate--right-top js-up js-right'>up/right</span>
-          <span className='tile__navigate tile__navigate--right js-right'>right</span>
-          <h1>Down Left</h1>
+          <span className='tile__navigate tile__navigate--up js-up'>cases</span>
+          <span className='tile__navigate tile__navigate--right-top js-up js-right'>main</span>
+          <span className='tile__navigate tile__navigate--right js-right'>we use</span>
+          <h1>let&apos;s start</h1>
         </div>
-        {/* 7 */}
+        {/* left */}
         <div className='tile' data-x-position='-1' data-y-position='0'>
-          <span className='tile__navigate tile__navigate--up js-up'>up</span>
-          <span className='tile__navigate tile__navigate--right-top js-up js-right'>up/right</span>
-          <span className='tile__navigate tile__navigate--right js-right'>right</span>
-          <span className='tile__navigate tile__navigate--right-down js-down js-right'>down/right</span>
-          <span className='tile__navigate tile__navigate--down js-down'>down</span>
-          <h1>Left</h1>
+          <span className='tile__navigate tile__navigate--up js-up'>about us</span>
+          <span className='tile__navigate tile__navigate--right-top js-up js-right'>we do</span>
+          <span className='tile__navigate tile__navigate--right js-right'>main</span>
+          <span className='tile__navigate tile__navigate--right-down js-down js-right'>we use</span>
+          <span className='tile__navigate tile__navigate--down js-down'>let&apos;s start</span>
+          <h1>cases</h1>
         </div>
-        {/* 8 */}
+        {/* up/left */}
         <div className='tile' data-x-position='-1' data-y-position='1'>
-          <span className='tile__navigate tile__navigate--right-down js-down js-right'>down/right</span>
-          <span className='tile__navigate tile__navigate--right js-right'>right</span>
-          <span className='tile__navigate tile__navigate--down js-down'>down</span>
-          <h1>Up Left</h1>
+          <span className='tile__navigate tile__navigate--right-down js-down js-right'>main</span>
+          <span className='tile__navigate tile__navigate--right js-right'>we do</span>
+          <span className='tile__navigate tile__navigate--down js-down'>cases</span>
+          <h1>about us</h1>
         </div>
       </div>
     </div>

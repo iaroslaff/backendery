e1adbd215e5f1d4687dafefa1aab6a66e12256c6
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-09-27 13:17:52 +0300

    feat (w3s): adapt the `UI` to browser specifics

--- packages/w3s/src/layouts/DefaultLayout.tsx
+++ packages/w3s/src/layouts/DefaultLayout.tsx
@@ -1,8 +1,25 @@
-import { FC } from "react"
+import { FC, useEffect } from "react"
 import { Outlet } from "react-router-dom"
 
 const DefaultLayout: FC = () => {
-  return (<Outlet />)
+  const VIEWPORT_UNIT = 0.01
+
+  useEffect(() => {
+    const setViewportDimensions = (): void => {
+      const rootStyle = document.documentElement.style
+      const { innerHeight, innerWidth } = window
+
+      rootStyle.setProperty("--vh", `${innerHeight * VIEWPORT_UNIT}px`)
+      rootStyle.setProperty("--vw", `${innerWidth * VIEWPORT_UNIT}px`)
+    }
+
+    setViewportDimensions()
+    window.addEventListener("resize", setViewportDimensions)
+
+    return () => window.removeEventListener("resize", setViewportDimensions)
+  }, [])
+
+  return <Outlet />
 }
 
 export default DefaultLayout

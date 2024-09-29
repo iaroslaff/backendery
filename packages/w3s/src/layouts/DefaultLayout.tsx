c3bc211b32b622e511bb24653d3546d1393067ca
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-09-29 15:30:18 +0300

    refactor (w3s): strip out unnecessary code

--- packages/w3s/src/layouts/DefaultLayout.tsx
+++ packages/w3s/src/layouts/DefaultLayout.tsx
@@ -1,24 +1,7 @@
-import { FC, useEffect } from "react"
+import { FC } from "react"
 import { Outlet } from "react-router-dom"
 
 const DefaultLayout: FC = () => {
-  const VIEWPORT_UNIT = 0.01
-
-  useEffect(() => {
-    const setViewportDimensions = (): void => {
-      const rootStyle = document.documentElement.style
-      const { innerHeight, innerWidth } = window
-
-      rootStyle.setProperty("--vh", `${innerHeight * VIEWPORT_UNIT}px`)
-      rootStyle.setProperty("--vw", `${innerWidth * VIEWPORT_UNIT}px`)
-    }
-
-    setViewportDimensions()
-    window.addEventListener("resize", setViewportDimensions)
-
-    return () => window.removeEventListener("resize", setViewportDimensions)
-  }, [])
-
   return <Outlet />
 }
 

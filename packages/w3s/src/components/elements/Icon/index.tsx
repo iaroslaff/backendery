538cb666a859a4a597cbda6c58bd62ca4ed773b4
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-10-03 12:31:19 +0300

    feat (w3s): add files and extend `SvgIcon` component

--- packages/w3s/src/components/elements/Icon/index.tsx
+++ packages/w3s/src/components/elements/Icon/index.tsx
@@ -2,10 +2,14 @@ import { FC, SVGProps } from "react"
 
 import { ReactComponent as PythonIcon } from "./icons/python.svg"
 import { ReactComponent as RustIcon } from "./icons/rust.svg"
+import { ReactComponent as GreenArrowRightIcon } from "./icons/green-arrow-right.svg"
+import { ReactComponent as ShowAllSpacesIcon } from "./icons/show-all-spaces.svg"
 
 const icons = {
+  "green-arrow-right": GreenArrowRightIcon,
   "python": PythonIcon,
-  "rust": RustIcon
+  "rust": RustIcon,
+  "show-all-spaces": ShowAllSpacesIcon
 } as const
 
 export type SvgIconName = keyof typeof icons

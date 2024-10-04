7743cbc667e273aae7fee8f18eb141c7a0e9160d
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-10-04 12:33:00 +0300

    chore (w3s): rename the classes and brought them to the correct `BEM` style

--- packages/w3s/src/containers/spaces/Main/Main.tsx
+++ packages/w3s/src/containers/spaces/Main/Main.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react"
 
-import { SvgIcon } from "@/components/elements/Icon"
+import { SvgIcon } from "../../../components/elements/Icon"
 
 import "./Main.scss"
 
@@ -9,11 +9,11 @@ interface IMainProps { zoomOut: (event: React.MouseEvent) => void; }
 
 const Main: FC<IMainProps> = ({ zoomOut }) => {
   return (
-    <div className='virtual-space__main'>
-      <div className='virtual-space__main__brand-name'>_backendery</div>
-      <div className='virtual-space__main__title'>Reliable backend for your projects_</div>
-      <div className='virtual-space__main__show-all-spaces' onClick={zoomOut}>
-        <SvgIcon name='show-all-spaces' />
+    <div className='virtual-space-main'>
+      <div className='virtual-space-main__brand-name'>_backendery</div>
+      <div className='virtual-space-main__title'>Reliable backend for your projects_</div>
+      <div className='virtual-space-main__show-all' onClick={zoomOut}>
+        <SvgIcon name='show-all' />
       </div>
     </div>
   )

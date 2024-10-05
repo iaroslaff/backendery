8a4cfb7b09fdd3b8b8afe062ae57db884f6b88ce
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-10-05 09:23:18 +0300

    chore (w3s): remove the `virtual-space-` prefix from class names

--- packages/w3s/src/containers/spaces/Main/Main.tsx
+++ packages/w3s/src/containers/spaces/Main/Main.tsx
@@ -9,10 +9,10 @@ interface IMainProps { zoomOut: (event: React.MouseEvent) => void; }
 
 const Main: FC<IMainProps> = ({ zoomOut }) => {
   return (
-    <div className='virtual-space-main'>
-      <div className='virtual-space-main__brand-name'>_backendery</div>
-      <div className='virtual-space-main__title'>Reliable backend for your projects_</div>
-      <div className='virtual-space-main__show-all' onClick={zoomOut}>
+    <div className='main'>
+      <div className='main__brand-name'>_backendery</div>
+      <div className='main__title'>Reliable backend for your projects_</div>
+      <div className='main__show-all' onClick={zoomOut}>
         <SvgIcon name='show-all' />
       </div>
     </div>

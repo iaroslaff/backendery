5c5d92884c691930b817c8065ae042fa602146cf
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-10-05 14:51:46 +0300

    refactor (w3s): perform a deep refactoring of the `Contacts` space

--- packages/w3s/src/containers/spaces/Contacs/Contacts.tsx
+++ packages/w3s/src/containers/spaces/Contacs/Contacts.tsx
@@ -1,56 +1,81 @@
-import { FC } from "react"
-import "./Contacts.scss"
+import React, { FC } from "react"
+import toast, { Toaster } from "react-hot-toast"
+
 import AnimateBinaryGrid from "../../../components/AnimateBinaryGrid/AnimateBinaryGrid"
-import { SvgIcon } from "@/components/elements/Icon"
+import { SvgIcon } from "../../../components/elements/Icon"
+import { generateRandomSpecialChars } from "../../../utils/fn"
 
-const Contacts: FC = () => { 
+import "./Contacts.scss"
+
+const RANDOM_SPECIAL_CHARS = 10 as number
+
+const Contacts: FC = () => {
   return (
-    <div className="virtual-space-contacts">
-
-      <h2 className="virtual-space-contacts__title">Contacts_</h2>
-
-      <div className="virtual-space-contacts__decor">
-        <div className="virtual-space-contacts__square"></div>
-        <p>{'*_<>***_{'}</p>
+    <div className='contacts'>
+      <h2 className='contacts__title'>Contacts_</h2>
+      <div className='contacts__decorative-wrapper'>
+        <div className='contacts__decorative-square'></div>
+        <p className='contacts__decorative-text'>{generateRandomSpecialChars(RANDOM_SPECIAL_CHARS)}</p>
       </div>
-
-      <div className="virtual-space-contacts__email-wrapper">
-        <p className="virtual-space-contacts__description">Not everybody has an experienced frontend developer</p>
-        <a className="virtual-space-contacts__email" href="">ask@backemdery.io</a>
-      </div>
-
-      <div className="virtual-space-contacts__social-wrapper">
-        <a className="virtual-space-contacts__social-link" href="">
-          Telegram
-          <SvgIcon
-            name="arrow-up"
-          />
-        </a>
-        <a className="virtual-space-contacts__social-link" href="">
-          Linkedin
-          <SvgIcon
-            name="arrow-up"
-          />
-        </a>
-        <a className="virtual-space-contacts__social-link" href="">
-          Facrbook
-          <SvgIcon
-            name="arrow-up"
-          />
-        </a>
-      </div>
-
-      <div className="virtual-space-contacts__decor-matrix-wrapper">
-        <AnimateBinaryGrid
-          symbols={['0', '1']}
-          rows={7}
-          cols={3}
-          minInterval={500}
-          maxInterval={1000}
-          unreachableCells={[[3, 1], [4, 1], [5, 1], [6, 1], [4, 2], [5, 2], [6, 2]]}
+      <div className='contacts__email-wrapper'>
+        <p className='contacts__email-description'>Not everybody has an experienced backend developer... write to us</p>
+        <p
+          className='contacts__email-address'
+          onClick={(event: React.MouseEvent): void => {
+            navigator.clipboard
+              .writeText(event.currentTarget.textContent?.trim() ?? "")
+              .then(() => {
+                toast("Copied!")
+              })
+              .catch(err => {
+                toast("Couldn't copy :/")
+                console.log("couldn't copy to clipboard", err)
+              })
+          }}
+        >
+          hey@backendery.dev
+        </p>
+        <Toaster
+          toastOptions={{
+            duration: 3000,
+            position: "bottom-center",
+            style: {
+              backgroundColor: "#ffffff",
+              borderRadius: 0,
+              color: "#141519",
+              animation: "none",
+            },
+          }}
         />
       </div>
-
+      <div className='contacts__social-wrapper'>
+        <a className='contacts__social-link' href='#'>
+          Telegram
+          <SvgIcon name='arrow-up' />
+        </a>
+        <a className='contacts__social-link' href='#'>
+          GitHub
+          <SvgIcon name='arrow-up' />
+        </a>
+      </div>
+      <div className='contacts__animate-binary-grid-wrapper'>
+        <AnimateBinaryGrid
+          symbols={["0", "1"]}
+          rows={7}
+          cols={3}
+          minInterval={300}
+          maxInterval={950}
+          unreachableCells={[
+            [3, 1],
+            [4, 1],
+            [5, 1],
+            [6, 1],
+            [4, 2],
+            [5, 2],
+            [6, 2],
+          ]}
+        />
+      </div>
     </div>
   )
 }

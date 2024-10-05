468e737b15c395333e2183c569360557c9ea3383
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-10-05 11:13:59 +0300

    chore (w3s): remove files and reduce `SvgIcon` component

--- packages/w3s/src/components/elements/Icon/index.tsx
+++ packages/w3s/src/components/elements/Icon/index.tsx
@@ -2,18 +2,13 @@ import { FC, SVGProps } from "react"
 
 import { ReactComponent as ArrowRightIcon } from "./icons/arrow-right.svg"
 import { ReactComponent as ArrowTurnIcon } from "./icons/arrow-turn.svg"
-import { ReactComponent as PythonIcon } from "./icons/python.svg"
-import { ReactComponent as RustIcon } from "./icons/rust.svg"
 import { ReactComponent as ShowAllIcon } from "./icons/show-all.svg"
 import { ReactComponent as ArrowUpIcon } from "./icons/arrow-up.svg"
 
 const icons = {
   "arrow-right": ArrowRightIcon,
   "arrow-turn": ArrowTurnIcon,
-  "python": PythonIcon,
-  "rust": RustIcon,
-  "show-all": ShowAllIcon,
-  "arrow-up": ArrowUpIcon
+  "show-all": ShowAllIcon
 } as const
 
 export type SvgIconName = keyof typeof icons

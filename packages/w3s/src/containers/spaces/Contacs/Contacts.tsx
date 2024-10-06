2e478616dc3ae3febdb4ef53d49870c07d6617af
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-10-06 16:02:29 +0300

    refactor (w3s): rewrite the chars animation code

--- packages/w3s/src/containers/spaces/Contacs/Contacts.tsx
+++ packages/w3s/src/containers/spaces/Contacs/Contacts.tsx
@@ -1,54 +1,62 @@
-import React, { FC, useEffect, useRef } from "react"
+import React, { FC, useEffect, useState } from "react"
 import toast, { Toaster } from "react-hot-toast"
-/* @ts-ignore */
-import shuffleLetters from "shuffle-letters"
+import { useScramble } from "use-scramble"
 
 import AnimateBinaryGrid from "../../../components/AnimateBinaryGrid/AnimateBinaryGrid"
 import { SvgIcon } from "../../../components/elements/Icon"
-import { generateRandomSpecialChars } from "../../../utils/fn"
+import { generateRandomChars } from "../../../utils/fn"
 
 import "./Contacts.scss"
 
-const RANDOM_SPECIAL_CHARS = (1 << 3) as number // 1 byte
-const SHUFFLE_LETTERS_INTERVAL = 9_500 as number
+const RANDOM_CHARS_NUMBER = (1 << 3) as number // 1 byte
+/* prettier-ignore */
+const RANDOM_CHARS_MAX_INTERVAL =  9_500 as number
+const RANDOM_CHARS_MIN_INTERVAL = 15_000 as number
 
 const Contacts: FC = () => {
-  /** refs */
-  const textRef = useRef<HTMLParagraphElement | null>(null)
+  /** states */
+  const [animationInterval, setAnimationInterval] = useState<number>(0)
+
+  /** hooks */
+  const { ref: textRef, replay } = useScramble({
+    text: generateRandomChars(RANDOM_CHARS_NUMBER),
+    speed: 0.65,
+    scramble: 35,
+    overflow: true,
+    overdrive: false,
+    onAnimationStart: () => {
+      /** function to generate a random interval between updates */
+      const randomInterval = () => {
+        return (
+          Math.floor(Math.random() * (RANDOM_CHARS_MAX_INTERVAL - RANDOM_CHARS_MIN_INTERVAL + 1)) +
+          RANDOM_CHARS_MIN_INTERVAL
+        )
+      }
+
+      /** generate an interval for animating chars */
+      const interval = randomInterval()
+
+      setAnimationInterval(interval)
+    },
+    onAnimationFrame: () => {
+      textRef.current.textContent = generateRandomChars(RANDOM_CHARS_NUMBER)
+    },
+  })
 
   useEffect(() => {
-    /** initial chars set */
-    const initialChars = generateRandomSpecialChars(RANDOM_SPECIAL_CHARS)
-    if (textRef.current) {
-      textRef.current.textContent = initialChars
-    }
+    /** set an interval for animating chars */
+    const interval = setInterval(replay, animationInterval)
 
-    const intervalId = setInterval(() => {
-      /** generating a new chars set */
-      const chars = generateRandomSpecialChars(RANDOM_SPECIAL_CHARS)
-
-      if (textRef.current) {
-        /** update element text before animation */
-        textRef.current.textContent = chars
-        /** run animation shuffleLetters */
-        shuffleLetters(textRef.current, {
-          fps: 12,
-          iterations: 20,
-        })
-      }
-    }, SHUFFLE_LETTERS_INTERVAL)
-
-    return () => clearInterval(intervalId)
-  }, [])
+    /** clear the interval when the component is unmounted */
+    return () => clearInterval(interval)
+  }, [animationInterval])
 
   return (
     <div className='contacts'>
       <h2 className='contacts__title'>Contacts_</h2>
       <div className='contacts__decorative-wrapper'>
         <div className='contacts__decorative-square'></div>
-        <p className='contacts__decorative-text' ref={textRef}>
-          {textRef.current?.textContent}
-        </p>
+        <p className='contacts__decorative-text' ref={textRef}></p>
       </div>
       <div className='contacts__email-wrapper'>
         <p className='contacts__email-description'>Not everybody has an experienced backend developer... write to us</p>

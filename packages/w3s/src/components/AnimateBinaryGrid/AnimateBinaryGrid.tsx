b7e1b44b90930c5262a4544b345e41a68e5aabd4
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-10-08 21:00:57 +0300

    refactor (w3s): perform a deep refactoring of the `AnimateBinaryGrid` component

--- packages/w3s/src/components/AnimateBinaryGrid/AnimateBinaryGrid.tsx
+++ packages/w3s/src/components/AnimateBinaryGrid/AnimateBinaryGrid.tsx
@@ -1,23 +1,60 @@
-import React, { FC, memo, useEffect, useState } from "react"
+import React, { FC, memo, useEffect, useRef, useState } from "react"
+
+import { randomInterval } from "../../utils/fn"
 
 import "./AnimateBinaryGrid.scss"
 
+/**
+ * define the props for the AnimateBinaryGrid component
+ */
 interface IAnimateBinaryGridProps {
+  /**
+   * @property {string[]} array of symbols to display
+   */
   symbols: string[]
+
+  /**
+   * @property {number} number of rows in the grid
+   */
   rows: number
+
+  /**
+   * @property {number} number of columns in the grid
+   */
   cols: number
+
+  /**
+   * @property {number} minimum interval for cell updates
+   */
   minInterval: number
+
+  /**
+   * @property {number} maximum interval for cell updates
+   */
   maxInterval: number
-  /* optional array of cell coordinates that should be unreachable (not updated) */
+
+  /**
+   * @property {Array<[number, number]>} optional array of cell coordinates that should not be updated
+   */
   unreachableCells?: Array<[number, number]>
+
+  /**
+   * @property {React.CSSProperties} optional additional styles for the grid
+   */
   style?: React.CSSProperties
 }
 
+/**
+ * сomponent for individual cells in the binary grid
+ */
 const AnimateBinaryCell: FC<{ symbol: string; isUnreachable: boolean }> = memo(({ symbol, isUnreachable }) => {
   /** each cell displays a symbol and applies a "unreachable" class if it is in a unreachable state */
   return <div className={`animate-binary-grid__cell ${isUnreachable ? "unreachable" : ""}`}>{symbol}</div>
 })
 
+/**
+ * main component for the animated binary grid
+ */
 const AnimateBinaryGrid: FC<IAnimateBinaryGridProps> = props => {
   /** validate unreachableCells to ensure that they are within bounds of the grid */
   props.unreachableCells?.forEach(([row, col]) => {
@@ -26,80 +63,96 @@ const AnimateBinaryGrid: FC<IAnimateBinaryGridProps> = props => {
     }
   })
 
-  /** creates the initial grid with randomly selected symbols from the symbol list */
+  /**
+   * function to create the initial grid with randomly selected symbols
+   */
   const createInitialCells = () => {
     return Array.from(
-      { length: props.rows * props.cols },
-      () => props.symbols[Math.floor(Math.random() * props.symbols.length)]
+      { length: props.rows * props.cols }, // create an array of the total number of cells
+      () => props.symbols[Math.floor(Math.random() * props.symbols.length)] // select a random symbol for each cell
     )
   }
 
   /** states */
+  /** state to hold the current symbols in the grid */
   const [cells, setCells] = useState<string[]>(createInitialCells())
 
-  /** convert unreachableCells to a Set to quickly check whether a cell is unreachable */
+  /** refs */
+  /** store the last update time for controlling animation timing */
+  const lastTimeRef = useRef<number | null>(null)
+
+  /** set of unreachable cell indices */
   const unreachableSet = new Set(props.unreachableCells?.map(([row, col]) => row * props.cols + col))
 
+  /**
+   * function to update a random cell with a new symbol
+   */
   const updateRandomCell = () => {
     let randomIndex = -1
 
     /** find a random index that is not a unreachable cell */
     do {
-      randomIndex = Math.floor(Math.random() * cells.length)
-    } while (unreachableSet.has(randomIndex))
+      randomIndex = Math.floor(Math.random() * cells.length) // get a random index
+    } while (unreachableSet.has(randomIndex)) // ensure the index is not for an unreachable cell
 
+    /** generate a new random symbol */
     const newSymbol = generateRandomSymbol()
 
-    /** update the symbol at the randomly selected index */
+    /** update the state with the new symbol */
     setCells(prevCells => {
       const newCells = [...prevCells]
-      newCells[randomIndex] = newSymbol
-
+      newCells[randomIndex] = newSymbol // update the selected cell
       return newCells
     })
   }
 
+  /**
+   * function to generate a new random symbol from the list, with no restriction against
+   * reusing the current symbol
+   */
   const generateRandomSymbol = () => {
-    /**
-     * generate a new random symbol from the list, with no restriction against
-     * reusing the current symbol
-     */
     return props.symbols[Math.floor(Math.random() * props.symbols.length)]
   }
 
   /** starts the random cell update process when the component mounts */
   useEffect(() => {
-    let intervalId: NodeJS.Timeout
+    /** animation loop that updates cells at random intervals */
+    const step = (currentTime: number) => {
+      if (lastTimeRef.current === null) {
+        lastTimeRef.current = currentTime // initialize the last update time
+      }
 
-    const setRandomInterval = () => {
-      const randomInterval = Math.floor(Math.random() * (props.maxInterval - props.minInterval + 1)) + props.minInterval
+      const elapsed = currentTime - lastTimeRef.current // calculate elapsed time since last update
+      const nextInterval = randomInterval(props.minInterval, props.maxInterval) // get a random interval between min and max
 
-      /** clear the previous interval, if any */
-      clearInterval(intervalId)
-      intervalId = setInterval(updateRandomCell, randomInterval) // create new interval
+      /** if enough time has elapsed, update a random cell */
+      if (elapsed >= nextInterval) {
+        updateRandomCell()
+        lastTimeRef.current = currentTime // reset the last update time
+      }
+
+      requestAnimationFrame(step) // continue the animation loop
     }
 
-    /** call for the first interval */
-    setRandomInterval()
+    /** start the animation */
+    const animationFrame = requestAnimationFrame(step)
 
-    return () => {
-      clearInterval(intervalId) // clean up the timeout when the component unmounts
-    }
-  }, [props.symbols, props.minInterval, props.maxInterval])
-
+    /** clean up the animation on component unmount */
+    return () => cancelAnimationFrame(animationFrame)
+  }, [props.symbols, props.minInterval, props.maxInterval]) // re-run the effect if props change
 
   return (
     <div
       className='animate-binary-grid'
       style={{
-        gridTemplateColumns: `repeat(${props.cols}, 1fr)`,
-        ...props.style,
+        gridTemplateColumns: `repeat(${props.cols}, 1fr)`, // set the grid layout based on columns
+        ...props.style, // spread any additional styles
       }}
     >
       {cells.map((symbol, index) => (
         <AnimateBinaryCell
           key={index}
-          symbol={unreachableSet.has(index) ? "" : symbol} // if the cell is unreachable, display an empty string
+          symbol={unreachableSet.has(index) ? "" : symbol} // show an empty string if the cell is unreachable
           isUnreachable={unreachableSet.has(index)} // pass the unreachable state to the cell
         />
       ))}

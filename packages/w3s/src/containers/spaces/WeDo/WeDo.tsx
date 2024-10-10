0cdb1daa309868b052137a90ba565e4d9f9980f3
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-10-10 21:36:33 +0300

    refactor (w3s): perform a refactoring of the `WeDo` space

--- packages/w3s/src/containers/spaces/WeDo/WeDo.tsx
+++ packages/w3s/src/containers/spaces/WeDo/WeDo.tsx
@@ -1,66 +1,152 @@
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
 import { ReactTyped as Typed } from "react-typed"
+import { useScramble } from "use-scramble"
+
+import AnimateRadixGrid from "../../../components/AnimateRadixGrid/AnimateRadixGrid"
+import AnimateSignalStrip from "../../../components/AnimateSignalStrip/AnimateSignalStrip"
 
 import "./WeDo.scss"
 
+/**
+ * An array of content objects representing the services offered by the company.
+ * Each object contains details about a specific service, including its identifier,
+ * visual symbol, name, and a description that outlines the service's features and benefits.
+ *
+ * @type {Array<WeDoContent>}
+ * @constant
+ *
+ * Each service object includes:
+ * - id: A unique identifier for the service.
+ * - symbol: A visual symbol representing the service.
+ * - name: The name of the service.
+ * - description: A detailed description of the service's purpose and benefits.
+ */
+const __weDoContent: Record<string, string>[] = [
+  {
+    id: "server-apps-and-api",
+    symbol: "=>",
+    name: "Server Apps & API",
+    description:
+      "We develop high-performance server applications and APIs that ensure reliable interaction between systems. Our solutions are tailored to meet specific business needs, enhancing operational efficiency and scalability",
+  },
+  {
+    id: "services-integration",
+    symbol: "@;",
+    name: "Services Integration",
+    description:
+      "We integrate diverse services to create seamless and effective workflows. By ensuring compatibility and efficiency, we help businesses streamline their processes and improve overall productivity",
+  },
+  {
+    id: "cli-and-automation-tools",
+    symbol: "&*",
+    name: "CLI & Automation Tools",
+    description:
+      "We create command-line tools and automation solutions to simplify routine tasks and boost productivity. Our tools are designed to enhance user experience, allowing teams to focus on more strategic initiatives",
+  },
+  {
+    id: "bots",
+    symbol: "==",
+    name: "Bots",
+    description:
+      "We develop bots for various platforms, including chatbots and user interaction automation. These solutions enhance customer experience and engagement, providing quick responses and improving service quality",
+  },
+] as const
+
+const INITIAL_ACTIVE_TAB = 0 as number
+
 const WeDo: FC = () => {
+  /** states */
+  const [activeTab, setActiveTab] = useState<string>(__weDoContent[INITIAL_ACTIVE_TAB].id)
+  const [description, setDescription] = useState<string>(__weDoContent[INITIAL_ACTIVE_TAB].description)
+
+  const { ref: descriptionRef, replay: scrambleReplay } = useScramble({
+    text: description,
+    speed: 0.85,
+    scramble: 3,
+    step: 5,
+    seed: 3,
+    overflow: true,
+    overdrive: false,
+    playOnMount: false,
+  })
+
+  useEffect(() => {
+    const currentContent = __weDoContent.find(wd => wd.id === activeTab)
+    if (currentContent) {
+      setDescription(currentContent.description)
+    }
+  }, [activeTab])
+
   return (
-    <div className="wedo">
-      <h2 className="wedo__title">
+    <div className='wedo'>
+      <h2 className='wedo__title'>
         <Typed strings={["We Do"]} typeSpeed={50} cursorChar='_' showCursor={true} startWhenVisible />
       </h2>
-      <div className="wedo__decorative-square">
+      <div className='wedo__decorative-square'></div>
+      <div className='wedo__tabs-wrapper'>
+        {__weDoContent.map(wd => (
+          <div
+            key={wd.id}
+            className='wedo__tab'
+            onClick={() => {
+              setActiveTab(wd.id)
+              setDescription(wd.description)
+              scrambleReplay()
+            }}
+          >
+            <p className='wedo__tab-symbol'>{wd.symbol}</p>
+            <p className={`wedo__tab-name ${activeTab === wd.id ? "active" : ""}`}>{wd.name}</p>
+          </div>
+        ))}
       </div>
-
-      <div className="wedo__tabs-container">
-        <div className="wedo__tabs-container__tab">
-          <p className="wedo__tabs-container__tab__symbol">{'=>'}</p>
-          <p className="wedo__tabs-container__tab__name">Server App</p>
+      <div className='wedo__description-wrapper'>
+        <div className='wedo__dots'>
+          <AnimateSignalStrip
+            symbol='.'
+            maxNumberOfSymbols={7}
+            minInterval={1_250}
+            maxInterval={2_500}
+            initialSymbols='.....'
+            style={{ color: "#67df8f" }}
+          />
+          <AnimateSignalStrip
+            symbol='.'
+            maxNumberOfSymbols={4}
+            minInterval={1_000}
+            maxInterval={2_500}
+            initialSymbols='..'
+            style={{ color: "#ffffff" }}
+          />
         </div>
-        <div className="wedo__tabs-container__tab">
-          <p className="wedo__tabs-container__tab__symbol">{'??'}</p>
-          <p className="wedo__tabs-container__tab__name">Services Integration</p>
-        </div>
-        <div className="wedo__tabs-container__tab">
-          <p className="wedo__tabs-container__tab__symbol">{'||'}</p>
-          <p className="wedo__tabs-container__tab__name">Api</p>
-        </div>
-        <div className="wedo__tabs-container__tab">
-          <p className="wedo__tabs-container__tab__symbol">{'&&'}</p>
-          <p className="wedo__tabs-container__tab__name">Automation Tools</p>
-        </div>
-        <div className="wedo__tabs-container__tab">
-          <p className="wedo__tabs-container__tab__symbol">{'=='}</p>
-          <p className="wedo__tabs-container__tab__name">Bots</p>
-        </div>
-      </div>
-
-      <div className="wedo__description-container">
-        <p className="wedo__description-container__animation-dots">..... <br /> ...</p>
-        <p className="wedo__description-container__description">
-          <span className="wedo__description-container__description__highlighter">*/ </span>
-          Not everybody has an experienced frontend developer on their team.
-          By Reliable backend for your projects joining our Discord server you can
-          pick my brain with any frontend related questions.
-          These one-on-one sessions give you direct access to my time and knowledge
-          <span className="wedo__description-container__description__highlighter"> */</span>
+        <p className='wedo__description' ref={descriptionRef}>
+          <span className='wedo__description-highlight'>/* </span>
+          {description}
+          <span className='wedo__description-highlight'> */</span>
         </p>
-        <div className="wedo__description-container__list">
-          <p className="wedo__description-container__list__item">{'Learning to find.............[ ok ]'}</p>
-          <p className="wedo__description-container__list__item">{'Mentorships..................[ ok ]'}</p>
-          <p className="wedo__description-container__list__item">{'Access to....................[ ok ]'}</p>
+        <div className='wedo__decorative-stdout'>
+          <p className='wedo__decorative-stdout--item'>{"Learning to find.............[ OK ]"}</p>
+          <p className='wedo__decorative-stdout--item'>{"Mentorships..................[ OK ]"}</p>
+          <p className='wedo__decorative-stdout--item'>{"Access to....................[ OK ]"}</p>
         </div>
       </div>
-
-      <div className="wedo__decorative-symbols">../../</div>
-      <div className="wedo__matrix">
-        <p>011001</p>
-        <p>0100100100</p>
+      <div className='wedo__decorative-symbols'>../../</div>
+      <div className='wedo__animate-radix-grid-wrapper'>
+        <AnimateRadixGrid
+          symbols={["0", "1"]}
+          rows={7}
+          cols={2}
+          minInterval={700}
+          maxInterval={1_150}
+          unreachableCells={[
+            [3, 0],
+            [4, 0],
+            [5, 0],
+            [6, 0],
+          ]}
+        />
       </div>
-
     </div>
   )
 }
 
 export default WeDo
-

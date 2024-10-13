d8dcd6adf4c70efd7d4c847b3736bb2e4d9cc001
Oleg Kirichok <kirichokoleg@gmail.com>
2024-10-13 15:27:27 +0300

    feat (w3s): add the `WeUse` space

--- packages/w3s/src/containers/spaces/WeUse/WeUse.tsx
+++ packages/w3s/src/containers/spaces/WeUse/WeUse.tsx
@@ -37,13 +37,155 @@ const WeUse: FC = () => {
 
   return (
     <div className='we-use'>
-      {
-        content?.map((card: CardContent, index: number) => (
-          <div className='box' key={index}>
-            {card.html}
-          </div>
-        ))
-      }
+      <h2 className="we-use__title">We Use_</h2>
+      <div className="we-use__random-symbols">{'[*&&/]'}</div>
+      <div className="we-use__decorative-square"></div>
+
+      <div className="we-use__tabs-group">
+        <button className="we-use__tab">Languages</button>
+        <button className="we-use__tab">Frameworks</button>
+        <button className="we-use__tab">Documentation</button>
+        <button className="we-use__tab">Containerization</button>
+        <button className="we-use__tab">Database</button>
+        <button className="we-use__tab">Message queues</button>
+        <button className="we-use__tab">Testing</button>
+      </div>
+
+      <div className="we-use__stack-adaptive">
+        <h3 className="we-use__stack-title">Frameworks</h3>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Kubernetes
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...GitLab
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...GitKraken
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...New Relic
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Datadog
+        </p>
+      </div>
+
+      <div className="we-use__stack">
+        <h3 className="we-use__stack-title">Languages</h3>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Python
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Rust
+        </p>
+      </div>
+
+      <div className="we-use__stack">
+        <h3 className="we-use__stack-title">Frameworks</h3>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Kubernetes
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...GitLab
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...GitKraken
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...New Relic
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Datadog
+        </p>
+      </div>
+
+      <div className="we-use__stack">
+        <h3 className="we-use__stack-title">Database</h3>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Back4App 
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Stack Overflow
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Postman API
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Firebase
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Apache
+        </p>
+      </div>
+
+      <div className="we-use__stack">
+        <h3 className="we-use__stack-title">Message queues</h3>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...GitHub
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Stack Overflow
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Docker
+        </p>
+      </div>
+
+      <div className="we-use__stack">
+        <h3 className="we-use__stack-title">Documentation</h3>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Heroku
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Visual Studio
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Jira
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...GitHub
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Bitbucket
+        </p>
+      </div>
+
+      <div className="we-use__stack">
+        <h3 className="we-use__stack-title">Containerization</h3>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...NGINX
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Apache
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Back4App
+        </p>
+      </div>
+
+      <div className="we-use__stack">
+        <h3 className="we-use__stack-title">Testing</h3>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Kubernetes
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Docker
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...Pivotal Tracker
+        </p>
+        <p className="we-use__stack-tool">
+          <span className="we-use__stack-tool--highlighter">{'>'}</span> ...GitHub
+        </p>
+      </div>
+
+      <div className="we-use__decorative-rectangle"></div>
+
+      <p className="we-use__description">
+        We cover the full range of services for analysis, development
+        and support of your online business
+      </p>
+
     </div>
   )
 }

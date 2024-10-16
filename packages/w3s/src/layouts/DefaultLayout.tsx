90a02ba05369e1a9bab712635a5b55d35c159bd7
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-10-16 18:01:02 +0300

    feat (w3s): apply the `OrientationHold` container (with conditions!)

--- packages/w3s/src/layouts/DefaultLayout.tsx
+++ packages/w3s/src/layouts/DefaultLayout.tsx
@@ -1,8 +1,19 @@
-import { FC } from "react"
+import React, { FC } from "react"
 import { Outlet } from "react-router-dom"
 
+import OrientationHold from "../containers/OrientationHold/OrientationHold"
+import { useBreakpoints } from "../hooks/useBreakpoints"
+
 const DefaultLayout: FC = () => {
-  return <Outlet />
+  // The useBreakpoints hook de-structurization
+  const { isSmartphone, isSmallDevice, isTablet } = useBreakpoints()
+
+  return (
+    <React.Fragment>
+      {(isSmartphone || isSmallDevice || isTablet) && <OrientationHold />}
+      <Outlet />
+    </React.Fragment>
+  )
 }
 
 export default DefaultLayout

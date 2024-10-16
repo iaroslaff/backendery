a6abfb10a0a66a88070404a951f36aded10d2f38
Jaroslav Kirichok <kirichok.jaroslav@gmail.com>
2024-10-16 11:12:25 +0300

    chore (w3s): change the names of classes, variables and constants

--- packages/w3s/src/containers/spaces/Contacs/Contacts.tsx
+++ packages/w3s/src/containers/spaces/Contacs/Contacts.tsx
@@ -10,11 +10,10 @@ import { randomBetween, randomChars, runWithTimeout } from "../../../utils/fn"
 
 import "./Contacts.scss"
 
-const CHARS_SEQUENCE = "1234567890ABCDEF" as string
-const RANDOM_CHARS_NUMBER = (1 << 3) as number
+const charsSequence = "1234567890ABCDEF" as string
+const randomCharsNumber = (1 << 3) as number
 
-const SCRAMBLE_PARAMS = {
-  text: `0x${randomChars(CHARS_SEQUENCE, RANDOM_CHARS_NUMBER)}`,
+const scrambleDecorativeTextParams = {
   speed: 0.45,
   tick: 1,
   step: 1,
@@ -29,17 +28,18 @@ const Contacts: FC = () => {
   const scrambleTimeoutRef = useRef<number | null>(null) // Ref to animation timeout for text
   const squareTimeoutRef = useRef<number | null>(null) // Ref to animation timeout for square
 
-  const { ref: textRef, replay: scrambleReplay } = useScramble({
+  const { ref: scrambleDecorativeTextRef, replay: scrambleReplay } = useScramble({
+    text: `0x${randomChars(charsSequence, randomCharsNumber)}`,
     ignore: ["0", "x"],
     range: [48, 57, 65, 70],
     onAnimationEnd: () => {
       const timeout = randomBetween(4_100, 7_550)
       runWithTimeout(scrambleTimeoutRef, scrambleReplay, timeout)
     },
-    ...SCRAMBLE_PARAMS
+    ...scrambleDecorativeTextParams,
   })
 
-  const { ref: squareRef, replay: squareReplay } = useRotator({
+  const { ref: decorativeSquareRef, replay: squareReplay } = useRotator({
     duration: 1_100,
     randomizeRotation: true,
     onAnimationEnd: () => {
@@ -78,8 +78,8 @@ const Contacts: FC = () => {
         <Typed strings={["Contacts"]} typeSpeed={50} cursorChar='_' showCursor={true} startWhenVisible />
       </h2>
       <div className='contacts__decorative-wrapper'>
-        <div className='contacts__decorative-square' ref={squareRef}></div>
-        <p className='contacts__decorative-text' ref={textRef}></p>
+        <div className='contacts__decorative-square' ref={decorativeSquareRef}></div>
+        <p className='contacts__decorative-text' ref={scrambleDecorativeTextRef}></p>
       </div>
       <div className='contacts__email-wrapper'>
         <p className='contacts__email-description'>Not everybody has an experienced backend developer... write to us</p>
@@ -122,7 +122,7 @@ const Contacts: FC = () => {
           <SvgIcon name='arrow-up' />
         </a>
       </div>
-      <div className='contacts__animate-radix-grid-wrapper'>
+      <div className='contacts__decorative-animate-radix-grid-wrapper'>
         <AnimateRadixGrid
           symbols={["0", "1"]}
           rows={7}
